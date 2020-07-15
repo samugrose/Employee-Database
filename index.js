8efbf5aae080c188b99b490ba0cb5e2b61ecc11c
Sam Grose <samugrose@gmail.com>
2020-07-15 15:47:01 -0700

    added view data function to satisfy MVP

--- index.js
+++ index.js
@@ -19,3 +19,30 @@ connection.connect(function (err) {
 
     // connection.end();
 })
+
+const askUser = async () => {
+    const {userChoice} = await inquirer.prompt([
+                {
+                    type:'list',
+                    message: 'What would you like to do?',
+                    name:'userChoice',
+                    choices:['view data', 'add data','change data', 'exit']
+                }
+            ]);
+    
+            switch (userChoice) {
+                case 'view data':
+                    readData();
+                    break;
+                case 'add data':
+                    addData();
+                    break;
+                case 'change data':
+                    changeData();
+                    break;
+                default:
+                    console.log('See you later');
+                    connection.end();
+                    break;
+            }
+}
