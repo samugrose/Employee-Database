5d6870a35c4ac7422b5c761ce09d0c8519e847cc
Sam Grose <samugrose@gmail.com>
2020-07-15 14:39:22 -0700

    added connection for sql

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    // Your port; if not 3306
+    port: process.env.PORT || 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "password",
+    database: "employee_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+
+    // connection.end();
+})
